e06f19871c58a96c3296cc9206cee2ae900128c3
김영주 <yjk@marpple.com>
2020-10-21 18:46:58 +0900

    add query.sql

---
+++ query.sql
@@ -0,0 +1,41 @@
+DROP TABLE users;
+DROP TABLE products;
+DROP TABLE orders;
+DROP TABLE delivery;
+
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	full_name VARCHAR(50) NOT NULL,
+	date_of_birth TIMESTAMP NOT NULL,
+	address VARCHAR(200) NOT NULL,
+	phone INT NOT NULL
+);
+
+CREATE TABLE orders (
+	id SERIAL PRIMARY KEY,
+	user_id SERIAL REFERENCES users,
+	product_id SERIAL REFERENCES products
+);
+
+CREATE TABLE products (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	price MONEY NOT NULL DEFAULT 0,
+	image VARCHAR(200),
+	created_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE TABLE delivery (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(20) NOT NULL,
+	price MONEY NOT NULL DEFAULT 0,
+	order_no SERIAL REFERENCES orders
+);
+
+INSERT INTO users (full_name, date_of_birth, address, phone)
+VALUES ('김영주', DATE '1990-04-30', '서울', 01021845200);
+
+INSERT INTO products (name, price, image)
+VALUES ('iPhone 12', 1500000, '/images/product_image1.jpg');
+
+SELECT price::numeric FROM products;
